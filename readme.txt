049d66df03687752a63931323280e937b20b1201
Kouichirou Okada <kouichirou_okada@rainforest.tokyo>
2018-09-03 00:38:42 +0000

    iptablesコマンド変更

---
+++ readme.txt
@@ -0,0 +1,4 @@
+
+https://pypi.org/project/NetfilterQueue/
+
+apt-get install build-essential python-dev libnetfilter-queue-dev
--- setup.sh
+++ setup.sh
@@ -1,1 +1,2 @@
-sudo iptables -I INPUT -d $1 -j NFQUEUE --queue-num 2
+#sudo iptables -I INPUT -d $1 -p udp -dport 53 -j NFQUEUE --queue-num 2
+sudo iptables -I INPUT --protocol udp --dport 53 -j NFQUEUE --queue-num 2
